96d620bad0b21a6a0e09f13d2b809d5dcb6592e7
Ricardo Serrato B. <rsbse@hotmail.com>
2023-12-16 17:54:58 -0600

    Changes

--- CheatsheetLayout/app/src/main/java/com/thisisthetime/cheatsheetlayout/LayoutCheatSheet.kt
+++ CheatsheetLayout/app/src/main/java/com/thisisthetime/cheatsheetlayout/LayoutCheatSheet.kt
@@ -1,2 +1,288 @@
 package com.thisisthetime.cheatsheetlayout
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.thisisthetime.cheatsheetlayout.ui.theme.CheatSheetLayoutTheme
+
+
+@Composable
+fun Element(text: String) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .border(border = BorderStroke(2.dp, MaterialTheme.colorScheme.onPrimary))
+            .width(50.dp)
+            .height(50.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+@Composable
+fun ElementFillSize(text: String) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .border(border = BorderStroke(2.dp, MaterialTheme.colorScheme.onPrimary))
+            .fillMaxHeight()
+            .height(50.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+@Composable
+fun OneRowNthElements() {
+    Row() {
+        Element(text = "1")
+        Element(text = "2")
+        Element(text = ".")
+        Element(text = ".")
+        Element(text = "N")
+    }
+}
+
+
+@Composable
+fun OneColumnNthElements() {
+    Column() {
+        Element(text = "1")
+        Element(text = "2")
+        Element(text = ".")
+        Element(text = ".")
+        Element(text = "N")
+    }
+}
+
+@Composable
+fun ElementsCenteredInColumn() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Element(text = "1")
+        Element(text = "2")
+        Element(text = ".")
+        Element(text = "N")
+    }
+}
+
+@Composable
+fun ElementsCenteredInRow() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Element(text = "1")
+        Element(text = "2")
+        Element(text = ".")
+        Element(text = "N")
+    }
+}
+
+
+@Composable
+fun NthElementsXNthElements() {
+    Column() {
+        Row() {
+            Element(text = "1")
+            Element(text = "2")
+            Element(text = "3")
+        }
+
+        Row() {
+            Element(text = "1")
+            Element(text = "2")
+            Element(text = "3")
+        }
+
+        Row() {
+            Element(text = "1")
+            Element(text = "2")
+            Element(text = "3")
+        }
+    }
+
+}
+
+
+
+@Composable
+fun OtherArragementsInRow() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        //horizontalArrangement = Arrangement.SpaceBetween,
+        //horizontalArrangement = Arrangement.SpaceAround,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Element(text = "1")
+        Element(text = "2")
+    }
+}
+
+@Composable
+fun OtherAlignmentsInColumn() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        // Uncomment the one you need
+        //horizontalAlignment = Alignment.Start,
+        //horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.End,
+    ) {
+        Element(text = "1")
+        Element(text = "2")
+    }
+}
+
+
+@Composable
+fun NestedRowsInColumns() {
+    Column() {
+        Column {
+            Element("C1")
+            Element("C2")
+        }
+
+        Row() {
+            Element("R1")
+            Element("R2")
+            Element("R3")
+        }
+        Row() {
+            Element("R1")
+            Element("R2")
+            Element("R3")
+        }
+
+        Column {
+            Element("C1")
+            Element("C2")
+            Element("C3")
+        }
+    }
+}
+
+@Composable
+fun NestedColumnsInRows() {
+    Row() {
+        Row {
+            Element("R1")
+            Element("R2")
+        }
+
+        Column() {
+            Element("C1")
+            Element("C2")
+            Element("C3")
+        }
+        Column() {
+            Element("C1")
+            Element("C2")
+            Element("C3")
+        }
+
+        Row {
+            Element("R1")
+            Element("R2")
+        }
+    }
+}
+
+@Composable
+fun WidthRow() {
+    Column() {
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            ElementFillSize(text = "Max width")
+        }
+
+        Row(modifier = Modifier.width(200.dp)) {
+            ElementFillSize(text = "200dp")
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(0.5f)) {
+            ElementFillSize(text = "50%")
+        }
+    }
+}
+
+@Composable
+fun HeightColumn() {
+    Row() {
+
+        Column(modifier = Modifier.fillMaxHeight()) {
+            ElementFillSize(text = "Max width")
+        }
+
+        Column(modifier = Modifier.height(200.dp)) {
+            ElementFillSize(text = "200dp")
+        }
+
+        Column(modifier = Modifier.fillMaxHeight(0.5f)) {
+            ElementFillSize(text = "50%")
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun LayoutPreview() {
+    CheatSheetLayoutTheme {
+        Surface() {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(300.dp)
+                        .border(border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)),
+                    /* verticalArrangement = Arrangement.Center,
+                     horizontalAlignment = Alignment.CenterHorizontally*/
+                ) {
+
+                    HeightColumn()
+                }
+            }
+
+        }
+    }
+}
--- ExtractRefactorGeminiBard/app/src/main/java/com/thisisthetime/extractrefactorgeminibard/PersonalInfoScreen.kt
+++ ExtractRefactorGeminiBard/app/src/main/java/com/thisisthetime/extractrefactorgeminibard/PersonalInfoScreen.kt
@@ -1,2 +1,188 @@
 package com.thisisthetime.extractrefactorgeminibard
 
+
+
+import android.widget.Spinner
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.ui.unit.dp
+
+// Additional imports for specific components used
+
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.Modifier
+import java.time.LocalDate
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonalInfoScreen(
+    name: String = "",
+    onNameChange: (String) -> Unit,
+    lastName: String = "",
+    onLastNameChange: (String) -> Unit,
+    email: String = "",
+    onEmailChange: (String) -> Unit,
+    nationality: String = "",
+    onNationalityChange: (String) -> Unit,
+    birthDate: LocalDate? = null,
+    onBirthDateChange: (LocalDate?) -> Unit,
+    onSubmit: () -> Unit,
+) {
+    Surface {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = "Enter your personal information",
+                style = MaterialTheme.typography.bodyLarge,
+            )
+
+            NameField(
+                value = name,
+                onValueChange = onNameChange,
+            )
+
+            LastNameField(
+                value = lastName,
+                onValueChange = onLastNameChange,
+            )
+
+            EmailField(
+                value = email,
+                onValueChange = onEmailChange,
+            )
+
+            NationalityField(
+                value = nationality,
+                onValueChange = onNationalityChange,
+            )
+
+            BirthdateField(
+                birthDate = birthDate,
+                onBirthDateChange = onBirthDateChange,
+            )
+
+            SubmitButton(
+                onClick = onSubmit,
+            )
+        }
+    }
+}
+
+
+
+@Composable
+fun NameField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text("Name") },
+        modifier = modifier.fillMaxWidth(),
+        singleLine = true,
+    )
+}
+
+@Composable
+fun LastNameField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text("Last Name") },
+        modifier = modifier.fillMaxWidth(),
+        singleLine = true,
+    )
+}
+
+@Composable
+fun EmailField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text("Email") },
+        modifier = modifier.fillMaxWidth(),
+        singleLine = true,
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BirthdateField(
+    modifier: Modifier = Modifier,
+    birthDate: LocalDate? = null,
+    onBirthDateChange: (LocalDate?) -> Unit,
+) {
+    DatePicker(
+        state = DatePickerState(null, null, IntRange(1900, 2030), DisplayMode.Input) ,
+        title = { Text("Birthdate") },
+        modifier = modifier.fillMaxWidth()
+
+    )
+}
+
+@Composable
+fun SubmitButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text("Submit")
+    }
+}
+
+
+@Composable
+fun NationalityField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text("Nationality") },
+        modifier = modifier.fillMaxWidth(),
+        singleLine = true,
+    )
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PersonalInfoScreenPreview() {
+    PersonalInfoScreen(
+        name = "Jane Doe",
+        lastName = "Johnson",
+        email = "janedoe@example.com",
+        nationality = "Canada",
+        birthDate = null,
+        onNameChange = { /* Handle name change here */ },
+        onLastNameChange = { /* Handle last name change here */ },
+        onEmailChange = { /* Handle email change here */ },
+        onNationalityChange = { /* Handle nationality change here */ },
+        onBirthDateChange = { /* Handle birth date change here */ },
+        onSubmit = { /* Handle submit action here */ },
+    )
+}
