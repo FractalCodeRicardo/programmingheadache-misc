7c945ba1dac96ca9632ed60a0b2202626c83fec0
Ricardo Serrato <rsbse@hotmail.com>
2024-08-28 15:36:38 -0600

    Mapper

---
+++ advanced-tricks-reflection-csharp/Bar.cs
@@ -0,0 +1,6 @@
+namespace advanced_tricks_reflection_csharp;
+
+public class Bar {
+
+    public string Name { get; set; }
+}
--- advanced-tricks-reflection-csharp/Tricks.cs
+++ advanced-tricks-reflection-csharp/Tricks.cs
@@ -3,10 +3,12 @@ using System.Reflection;
 
 namespace advanced_tricks_reflection_csharp;
 
-public class Tricks {
+public class Tricks
+{
 
 
-    public void getNameWithLambda() {
+    public void getNameWithLambda()
+    {
 
         var prop = GetPropertyInfo(new Foo(), i => i.Name);
         Console.WriteLine(prop.Name);
@@ -16,30 +18,52 @@ public class Tricks {
     public static PropertyInfo GetPropertyInfo<TSource, TProperty>(
     TSource source,
     Expression<Func<TSource, TProperty>> propertyLambda)
-{
-    if (propertyLambda.Body is not MemberExpression member)
     {
-        throw new ArgumentException(string.Format(
-            "Expression '{0}' refers to a method, not a property.",
-            propertyLambda.ToString()));
+        if (propertyLambda.Body is not MemberExpression member)
+        {
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a method, not a property.",
+                propertyLambda.ToString()));
+        }
+
+        if (member.Member is not PropertyInfo propInfo)
+        {
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a field, not a property.",
+                propertyLambda.ToString()));
+        }
+
+        Type type = typeof(TSource);
+        if (propInfo.ReflectedType != null && type != propInfo.ReflectedType && !type.IsSubclassOf(propInfo.ReflectedType))
+        {
+            throw new ArgumentException(string.Format(
+                "Expression '{0}' refers to a property that is not from type {1}.",
+                propertyLambda.ToString(),
+                type));
+        }
+
+        return propInfo;
     }
 
-    if (member.Member is not PropertyInfo propInfo)
-    {
-        throw new ArgumentException(string.Format(
-            "Expression '{0}' refers to a field, not a property.",
-            propertyLambda.ToString()));
+
+    public void Mapper() {
+        var foo = new Foo();
+        var bar = Map<Foo, Bar>(foo);
     }
 
-    Type type = typeof(TSource);
-    if (propInfo.ReflectedType != null && type != propInfo.ReflectedType && !type.IsSubclassOf(propInfo.ReflectedType))
+      public static TTarget Map<TSource, TTarget>(TSource source)
+        where TTarget : new()
     {
-        throw new ArgumentException(string.Format(
-            "Expression '{0}' refers to a property that is not from type {1}.",
-            propertyLambda.ToString(),
-            type));
+        TTarget target = new TTarget();
+        foreach (PropertyInfo sourceProperty in typeof(TSource).GetProperties())
+        {
+            PropertyInfo targetProperty = typeof(TTarget).GetProperty(sourceProperty.Name);
+            if (targetProperty != null && targetProperty.CanWrite)
+            {
+                object value = sourceProperty.GetValue(source, null);
+                targetProperty.SetValue(target, value, null);
+            }
+        }
+        return target;
     }
-
-    return propInfo;
-}
 }
